a3d14367b3d721c80939172f0a576f4128c770bb
Craithtv <leonardpatrick12@gmail.com>
2023-04-02 14:57:02 -0400

    Added dynamic varibles
    Wine2 is serving as the "correct answer" and is now randomly generated out of an array of Possible Wines. Wine1 is now changeable via various buttons. After a button is clicked, a comparison between wine1/2 is made. This will serve as a skeleton for a future question/answer dynamic.

--- WineProto/Assets/Scripts/AnswerChecker.cs
+++ WineProto/Assets/Scripts/AnswerChecker.cs
@@ -6,13 +6,27 @@ using WineSim.Items;
 public class AnswerChecker : MonoBehaviour
 {
     public Wine wine1, wine2;
-
-
+    public ScriptableObject [] PossibleWines;
+    public ScriptableObject cabSauvObj; 
+    public ScriptableObject falanObj;
+    public ScriptableObject neroObj;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        if(wine1.varietal == wine2.varietal)
+        GetAnswerWine();
+    }
+
+    private void GetAnswerWine()
+    {
+        wine2 = PossibleWines[Random.Range(0, PossibleWines.Length)] as Wine;
+    }
+
+    private void CheckAnswer()
+    {
+        if (wine1.varietal == wine2.varietal)
         {
             Debug.Log("Varietal match");
         }
@@ -21,14 +35,15 @@ public class AnswerChecker : MonoBehaviour
             Debug.Log("Varietal does not match");
         }
 
-        if(wine1.origin == wine2.origin)
+        if (wine1.origin == wine2.origin)
         {
             Debug.Log("Origin match!");
         }
         else
-        {Debug.Log("Origin does not match");
+        {
+            Debug.Log("Origin does not match");
         }
-        if(wine1.color == wine2.color)
+        if (wine1.color == wine2.color)
         {
             Debug.Log("Color match");
         }
@@ -42,5 +57,21 @@ public class AnswerChecker : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnCabButton()
+    {
+        wine1 = cabSauvObj as Wine;
+        CheckAnswer();
+    }
+    public void OnFalButton()
+    {
+        wine1 = falanObj as Wine;
+        CheckAnswer();
+    }
+    public void OnNeroButton()
+    {
+        wine1 = neroObj as Wine;
+        CheckAnswer();
     }
 }
---
+++ WineProto/Assets/Scripts/WineEnums.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WineSim.Items
+{
+   public enum VarietalEnum 
+{
+    CabSauv,
+    Nero,
+    Falanghina,
+
+}
+public enum OriginEnum 
+{
+    USA,
+    Sicily,
+    Campania,
+
+}
+public enum ColorEnum 
+{
+   Red,
+   White,
+
+}
+
+}
